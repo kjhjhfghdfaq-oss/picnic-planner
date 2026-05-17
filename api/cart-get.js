a7d56009ee5d0023ee1cbf539d828bd8605c4849
Quinten Heerema <quintenheerema@distrivers.nl>
2026-05-17 19:51:02 +0200

    voeg cart-get endpoint toe

---
+++ api/cart-get.js
@@ -0,0 +1,29 @@
+﻿const https = require("https");
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") { res.status(200).end(); return; }
+  const { auth } = req.query;
+  const options = {
+    hostname: "storefront-prod.nl.picnicinternational.com",
+    path: "/api/15/cart",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      "x-client-version": "15.0",
+      "x-picnic-auth": auth || ""
+    }
+  };
+  const data = await new Promise((resolve, reject) => {
+    const r = https.request(options, resp => {
+      let d = "";
+      resp.on("data", c => d += c);
+      resp.on("end", () => resolve({ status: resp.statusCode, body: d }));
+    });
+    r.on("error", reject);
+    r.end();
+  });
+  res.status(data.status).send(data.body);
+};
